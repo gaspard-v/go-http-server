5fc74d8b793996f95acaa5dfb15a9841bcf3e65f
gaspard-v <60385905+gaspard-v@users.noreply.github.com>
2024-05-29 08:21:37 +0000

    Add OnConnected tcp client function

--- tcp/client/consumer.go
+++ tcp/client/consumer.go
@@ -3,6 +3,5 @@ package client
 import "net"
 
 type TcpClientConsumer interface {
-	Send(*net.TCPConn) uint64
-	OnReceive(*net.TCPConn) uint64
+	OnConnected(*net.TCPConn)
 }
--- tcp/client/tcp.go
+++ tcp/client/tcp.go
@@ -1,15 +1,31 @@
 package client
 
 import (
+	"fmt"
 	"net"
 
 	"github.com/gaspard-v/go-http-server/log"
 )
 
+const DEFAULT_ADDRESS string = "127.0.0.1"
+const DEFAULT_PORT string = "8080"
+
 type TcpClient struct {
-	conn              *net.TCPConn
+	tcpAddr           *net.TCPAddr
 	tcpClientConsumer TcpClientConsumer
 	logger            log.LogConsumerInterface
+}
+
+func CreateDefault(
+	tcpClientConsumer TcpClientConsumer,
+	logger log.LogConsumerInterface,
+) *TcpClient {
+	address := fmt.Sprintf("%s:%s", DEFAULT_ADDRESS, DEFAULT_PORT)
+	return Create(
+		address,
+		tcpClientConsumer,
+		logger,
+	)
 }
 
 func Create(
@@ -21,10 +37,14 @@ func Create(
 	if error != nil {
 		logger.Fatal(error)
 	}
-	conn, error := net.DialTCP("tcp", nil, tcpAddr)
-	if error != nil {
-		logger.Fatal(error)
-	}
-	tcpClient := TcpClient{conn, tcpConsumer, logger}
+	tcpClient := TcpClient{tcpAddr, tcpConsumer, logger}
 	return &tcpClient
+}
+
+func (tcpClient *TcpClient) Connect() {
+	conn, error := net.DialTCP("tcp", nil, tcpClient.tcpAddr)
+	if error != nil {
+		tcpClient.logger.Fatal(error)
+	}
+	go tcpClient.tcpClientConsumer.OnConnected(conn)
 }
